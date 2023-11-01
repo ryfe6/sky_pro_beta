3a9b1673eb8527b9b90a253304b422cd298e4eb1
ryfe <denis.koptelev@gmail.com>
2023-11-01 16:13:32 +0300

    fix test_processing

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -2,11 +2,8 @@ from src import processing
 
 
 def test_dictionary_list_filter(test_data_processing):
-    assert (processing.dictionary_list_filter(test_data_processing, "CANCELED") ==
-            [{'date': '2018-09-12T21:27:25.241689', 'id': 594226727, 'state': 'CANCELED'}])
-    assert (processing.dictionary_list_filter(test_data_processing) ==
-            [{'date': '2019-07-03T18:35:29.512364', 'id': 41428829, 'state': 'EXECUTED'},
-             {'date': '2018-06-30T02:08:58.425572', 'id': 939719570, 'state': 'EXECUTED'}])
+    assert processing.dictionary_list_filter(test_data_processing, "CANCELED") == var_3
+    assert processing.dictionary_list_filter(test_data_processing) == var_4
 
 
 def test_dictionary_list_filter_time(test_data_processing):
@@ -22,3 +19,8 @@ var = [{'date': '2019-07-03T18:35:29.512364', 'id': 41428829, 'state': 'EXECUTED
 var_2 = [{'date': '2018-06-30T02:08:58.425572', 'id': 939719570, 'state': 'EXECUTED'},
          {'date': '2018-09-12T21:27:25.241689', 'id': 594226727, 'state': 'CANCELED'},
          {'date': '2019-07-03T18:35:29.512364', 'id': 41428829, 'state': 'EXECUTED'}]
+
+var_3 = [{'date': '2018-09-12T21:27:25.241689', 'id': 594226727, 'state': 'CANCELED'}]
+
+var_4 = [{'date': '2019-07-03T18:35:29.512364', 'id': 41428829, 'state': 'EXECUTED'},
+         {'date': '2018-06-30T02:08:58.425572', 'id': 939719570, 'state': 'EXECUTED'}]
