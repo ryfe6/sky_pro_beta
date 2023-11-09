8770c53b47cef203ffa127c1f2c9a4f9c5d3d0f9
ryfe <denis.koptelev@gmail.com>
2023-11-09 14:07:00 +0300

    fix decorators and test_decorators

--- src/decorators.py
+++ src/decorators.py
@@ -3,32 +3,80 @@ from functools import wraps
 from typing import Any, Callable
 
 
-def log(filename: str) -> Callable:
+# def log(filename: str) -> Callable:
+#     """Декоратор, принимает путь для записи данных."""
+#
+#     def wrapper(func: Callable) -> Callable:
+#         """Декоратор, который выводит имя выполнения функции."""
+#         @wraps(func)
+#         def inner(a: int, b: int) -> Any:
+#             """Декоратор, который выполняет функцию"""
+#             try:
+#                 result = func(a, b)
+#             except ZeroDivisionError:
+#                 result = "ZeroDivisionError"
+#             now_time = time.localtime()
+#             current_time = time.strftime("%Y-%m-%dT%H:%M:%S", now_time)
+#
+#             if filename is not None:
+#                 with open(filename, "a", encoding="utf-8") as file:
+#                     if result != "ZeroDivisionError":
+#                         file.write(f"{current_time} my_function ok \n")
+#                     else:
+#                         file.write(f"{current_time} my_function error: <{result}>. Inputs: {a, b} \n")
+#             else:
+#                 if result != "ZeroDivisionError":
+#                     print(f"{current_time} my_function ok \n")
+#                 else:
+#                     print(f"{current_time} my_function error: <{result}>. Inputs: {a, b} \n")
+#             return result
+#
+#         return inner
+#
+#     return wrapper
+#
+#
+# @log(filename="mylog.txt")
+# def my_function(x: int, y: int) -> Any:
+#     """
+#     Функция принимает два числа и возвращает результат сложения двух чисел.
+#     :param x: Число.
+#     :param y: Число.
+#     :return: Результат.
+#     """
+#     return x / y
+#
+#
+# print(my_function(2, 1))
+
+def log(filename: Any = None) -> Callable:
     """Декоратор, принимает путь для записи данных."""
 
     def wrapper(func: Callable) -> Callable:
         """Декоратор, который выводит имя выполнения функции."""
+
         @wraps(func)
         def inner(a: int, b: int) -> Any:
             """Декоратор, который выполняет функцию"""
             try:
                 result = func(a, b)
-            except ZeroDivisionError:
-                result = "ZeroDivisionError"
+            except Exception as error:
+                error_try = error
+                result = None
             now_time = time.localtime()
-            current_time = time.strftime("%Y-%m-%dT%H:%M:%S", now_time)
+            current_time = time.strftime("%Y-%m-%d %H:%M:%S", now_time)
 
             if filename is not None:
                 with open(filename, "a", encoding="utf-8") as file:
-                    if result != "ZeroDivisionError":
+                    if result is not None:
                         file.write(f"{current_time} my_function ok \n")
                     else:
-                        file.write(f"{current_time} my_function error: <{result}>. Inputs: {a, b} \n")
+                        file.write(f"{current_time} my_function error: <{error_try}>. Inputs: {a, b} \n")
             else:
-                if result != "ZeroDivisionError":
+                if result is not None:
                     print(f"{current_time} my_function ok \n")
                 else:
-                    print(f"{current_time} my_function error: <{result}>. Inputs: {a, b} \n")
+                    print(f"{current_time} my_function error: <{error_try}>. Inputs: {a, b} \n")
             return result
 
         return inner
@@ -36,7 +84,7 @@ def log(filename: str) -> Callable:
     return wrapper
 
 
-@log(filename="mylog.txt")
+@log()
 def my_function(x: int, y: int) -> Any:
     """
     Функция принимает два числа и возвращает результат сложения двух чисел.
@@ -47,4 +95,4 @@ def my_function(x: int, y: int) -> Any:
     return x / y
 
 
-print(my_function(2, 1))
+print(my_function(2, 0))
--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,11 +1,43 @@
-from src import decorators
+import datetime
+
 import pytest
 
-
-@pytest.mark.parametrize("x, y, expected_result", [(1, 0, "ZeroDivisionError"),
-                                                   (2, 1, 2.0)])
-def test_my_function(x, y, expected_result):
-    assert decorators.my_function(x, y) == expected_result
+from src.decorators import log
 
 
+@pytest.mark.parametrize(
+    "a, b, expected_result",
+    [(1, 0, " my_function error: <division by zero>. Inputs: (1, 0)"), (2, 1, " my_function ok")],
+)
+def test_log_to_file(a, b, expected_result):
+    """Тест логирования в файл."""
+    filename = "mylog.txt"
 
+    @log(filename=filename)
+    def my_function(x, y):
+        return x / y
+
+    my_function(a, b)  # вызвали декорированную функцию
+
+    with open(filename, "r") as file:  # открыли файл и прочитали что в него записалось
+        for line in file:
+            pass
+        log_info = line.strip()
+
+    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    expected_log_info = f"{now}{expected_result}"
+    assert log_info == expected_log_info  # сверили результат с ожидаемым
+
+
+@pytest.mark.parametrize("a, b, expected_result", [(2, "1", None), (2, 2, 1.0)])
+def test_log_to_file_2(a, b, expected_result):
+    """Тест логирования в файл."""
+
+    @log()
+    def my_function(x, y):
+        return x / y
+
+    log_info = my_function(a, b)  # вызвали декорированную функцию
+
+    expected_log_info = expected_result
+    assert log_info == expected_log_info  # сверили результат с ожидаемым
